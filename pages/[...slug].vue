1489104c7592b301b018c9258df1a3930bdca8dc
bmpark <bmpark@jinhak.com>
2023-08-10 15:16:08 +0900

    test

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,6 +1,7 @@
 <template>
     <main>
         ddd
+        <nuxt-link to="/zz">zz</nuxt-link>
         <!-- <Co`ntentDoc v-slot="{ doc }">
       <h1>{{ doc.title }}</h1>
       <ContentRenderer :value="doc" />
