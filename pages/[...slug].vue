7815f7901e51c5cde7c604cd90509c1d69cd3d13
bmpark <bmpark@jinhak.com>
2023-08-10 17:27:08 +0900

    test

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -3,7 +3,7 @@
         ddd
         <nuxt-link to="/zz">zz</nuxt-link>
         <nuxt-link to="/zzt">zz</nuxt-link>
-        <nuxt-link to="/zee">zz</nuxt-link>
+        <nuxt-link to="/hello">zz</nuxt-link>
         <!-- <Co`ntentDoc v-slot="{ doc }">
       <h1>{{ doc.title }}</h1>
       <ContentRenderer :value="doc" />
